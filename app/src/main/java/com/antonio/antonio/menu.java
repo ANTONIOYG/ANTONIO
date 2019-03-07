d3587399608c347bea1080c64ea054d9d6d8ddff
ANTONIOYG <ANTONIOYG@HOTMAIL.COM>
2019-03-07 11:38:58 +0100

    segunda Activity

--- app/src/main/java/com/antonio/antonio/menu.java
+++ app/src/main/java/com/antonio/antonio/menu.java
@@ -2,12 +2,64 @@ package com.antonio.antonio;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.view.View;
 
-public class menu extends AppCompatActivity {
+public class menu extends AppCompatActivity implements View.OnClickListener{
+
+    EditText id,mes,dia,hentrada,hsalida,obra,compi;
+
+    Button buscar,bmodi,borrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        final Admin conn = new Admin(this);
+
+
+
+        id = (EditText) findViewById(R.id.tid);
+        mes = (EditText) findViewById(R.id.mes);
+        dia = (EditText) findViewById(R.id.dia);
+        hentrada = (EditText) findViewById(R.id.hentrada);
+        hsalida = (EditText) findViewById(R.id.hsalida);
+        obra = (EditText) findViewById(R.id.obra);
+        compi = (EditText) findViewById(R.id.compi);
+
+        buscar = (Button) findViewById(R.id.buscar);
+        bmodi = (Button) findViewById(R.id.bmodi);
+        borrar = (Button) findViewById(R.id.borrar);
+
+
+}
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.buscar:
+                Buscar();
+                break;
+            case R.id.bmodi:
+                aztualizar();
+                break;
+            case R.id.borrar:
+                Borrar();
+                break;
+        }
+
     }
+
+
+
+    private void Buscar() {
+    }
+
+    private void aztualizar(){
+    }
+
+    private void Borrar() {
+    }
+
 }
